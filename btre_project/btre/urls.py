e810f945b9bbc2269e28309d1f249b50bc70d299
Thato/ <t.j.mahlatji@gmail.com>
2023-02-13 18:27:29 +0200

    Admin Updated

--- btre_project/btre/urls.py
+++ btre_project/btre/urls.py
@@ -1,3 +1,4 @@
+# pylint: disable=import-error
 """btre URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -14,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('pages.urls')),
+    path('listings/', include('listings.urls')),
     path("admin/", admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
